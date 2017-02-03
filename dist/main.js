6bbc29821742b3987b551961680827f0211053c4
Dave Barner <dbarner@gmail.com>
2017-02-03 13:31:59 -0500

    moving BSLC to this repo

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,51 @@
+
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className)
+  else
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
+
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className)
+  else if (!hasClass(el, className)) el.className += " " + className
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className=el.className.replace(reg, ' ')
+  }
+}
+
+//WHY BOXES
+
+var featuredWhyBoxContents = document.querySelector(".why-box-featured");
+var whyBox1 = document.querySelector(".whyBox1");
+var whyBox2 = document.querySelector(".whyBox2");
+var whyBox3 = document.querySelector(".whyBox3");
+
+whyBox1.addEventListener("click", function() {
+  featuredWhyBoxContents.innerHTML = whyBox1.innerHTML;
+  featuredWhyBoxContents.classList +=" clickedWhy1 ";
+
+});
+
+whyBox2.addEventListener("click", function() {
+  featuredWhyBoxContents.innerHTML = whyBox2.innerHTML;
+  featuredWhyBoxContents.classList +=" clickedWhy2 ";
+
+});
+
+whyBox3.addEventListener("click", function() {
+  featuredWhyBoxContents.innerHTML = whyBox3.innerHTML;
+  featuredWhyBoxContents.classList +=" clickedWhy3 ";
+
+});
+
 //SMOOTH SCROLLING CODE
 
 $(document).ready(function(){
@@ -24,3 +72,26 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+//Banner
+
+var banner = document.querySelector(".bannerAnnouncement");
+var closeBannerButton = document.querySelector(".closeBanner");
+var headlineDiv = document.querySelector(".headline");
+var headlineDiv3 = document.querySelector(".headline h3");
+//var middleSubHeroDiv = document.querySelector(".middleSubHero");
+
+closeBannerButton.addEventListener("click", function() {
+  console.log("hey");
+    addClass(banner, "hide");
+    addClass(closeBannerButton, "hide");
+    removeClass(banner, "bannerAnnouncement");
+    removeClass(closeBannerButton, "closeBanner");
+    addClass(headlineDiv, "noBannerAnnouncement");
+    addClass(headlineDiv3, "noBannerAnnouncement");
+   // addClass(middleSubHeroDiv, "noBannerAnnouncement");
+})
+
+
+
+
